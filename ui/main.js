29f7b8b0ebd3162ca119fc756bd97396eab1dd63
ho9y <panwarhoney9@gmail.com>
2016-11-02 01:26:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,3 +13,11 @@ function moveRight(){
 img.onclick = function(){
     var interval = setInterval(moveRight,10);
 };
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick=function(){
+    
+    
+    
+    
+}
