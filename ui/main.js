0f85cc4db757a6cac75a3adcc030e9ca53a750af
ho9y <panwarhoney9@gmail.com>
2016-11-01 21:15:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
-element.innerHTML = 'New value'
+element.innerHTML = 'New value';
 var img = document.getElementById('madi');
 img.onclick=function(){
     img.style.marginLeft='100px';
