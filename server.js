3ccfdc679da5270b209b3b278cc8b0c5a1247067
ho9y <panwarhoney9@gmail.com>
2016-11-01 19:13:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -45,8 +45,6 @@ var htmlTemplate =
          return htmlTemplate;
          
 }
-app.get('/article-one',function(req,res){
-    res.send(createTemplate(articleOne));});
 
 
 
@@ -57,6 +55,9 @@ app.get('/article-one',function(req,res){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-one',function(req,res){
+    res.send(createTemplate(articleOne));});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
