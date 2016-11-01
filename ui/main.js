15b01aadf76310a9305a78ffb283f772919a9912
ho9y <panwarhoney9@gmail.com>
2016-11-02 01:02:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
 console.log('Loaded!');
-var element = document.getElementById('main-text');
+`var element = document.getElementById('main-text');
 element.innerHTML = "Stay tuned..";
 var img= document.getElementById('madi');
-
+`
 
 var marginLeft = 0;
 function moveRight(){
