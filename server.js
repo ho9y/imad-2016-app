298fb17dffaa05328db57015185aceb973bbb1ab
ho9y <panwarhoney9@gmail.com>
2016-11-06 03:29:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,7 +90,12 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-
+ 
+ app.get('/profile',function(req,res){
+   res.send('profile will be served here'); 
+     })
+     
+     
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
