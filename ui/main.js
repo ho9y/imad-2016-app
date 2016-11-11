ff84bc60ced949824800bb909a1348cff3a112ed
ho9y <panwarhoney9@gmail.com>
2016-11-12 02:57:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,10 @@
 console.log('Loaded!');
-`var element = document.getElementById('main-text');
+var element = document.getElementById('main-text');
 element.innerHTML = "Stay tuned..";
 var img= document.getElementById('madi');
-`
 
-`var marginLeft = 0;
+
+var marginLeft = 0;
 function moveRight(){
     marginLeft = marginLeft+1;
     img.style.marginLeft = marginLeft + 'px';
@@ -12,7 +12,7 @@ function moveRight(){
 
 img.onclick = function(){
     var interval = setInterval(moveRight,10);
-};`
+};
 
 var button = document.getElementById('counter');
 var counter = 0;
