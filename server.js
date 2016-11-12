4730ea5a059b389c12ecc8f77440a1838405a29f
ho9y <panwarhoney9@gmail.com>
2016-11-13 02:02:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ JavaScript Guide (this guide) provides an overview about the JavaScript language
 JavaScript Reference provides detailed reference material for JavaScript.
 If you are new to JavaScript, start with the articles in the learning area and the JavaScript Guide. Once you have a firm grasp of the fundamentals, you can use the JavaScript Reference to get more details on individual objects and statements.
 
-What is JavaScript?
+<h1>What is JavaScript?</h1>
 JavaScript is a cross-platform, object-oriented scripting language. It is a small and lightweight language. Inside a host environment (for example, a web browser), JavaScript can be connected to the objects of its environment to provide programmatic control over them.
 
 JavaScript contains a standard library of objects, such as Array, Date, and Math, and a core set of language elements such as operators, control structures, and statements. Core JavaScript can be extended for a variety of purposes by supplementing it with additional objects; for example:
